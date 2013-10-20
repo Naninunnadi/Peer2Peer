1ac287e33f51dce7fcce7915584208be29c329b7
Naninunnadi <mari-liis.saks@netgroup.ee>
2013-10-20 17:55:38 +0300

    File model

---
+++ WindowsFormsApplication1/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class FileModel
+    {
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+
+    }
+}
