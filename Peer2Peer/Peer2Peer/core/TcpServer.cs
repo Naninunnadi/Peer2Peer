adc233de032b5eed1500bfd91dc9b6a40662b56d
Ivo Pure <icoscx@gmail.com>
2013-10-26 15:51:34 +0300

    dfdfdfdfdfdfdfd

---
+++ Peer2Peer/Peer2Peer/core/TcpServer.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peer2Peer.core
+{
+    internal class TcpServer
+    {
+        public RichTextBox RichTextBox { get; set; }
+
+        public TcpServer(RichTextBox textBox)
+        {
+            this.RichTextBox = textBox;
+
+        }
+
+        public void TcpServerListen()
+        {
+
+            TcpListener server = null;
+            StringBuilder stringBuilder = new StringBuilder(1024*8);
+
+            try
+            {
+                String getip = utilities.Factory.LocalIPAddress();
+                String getport = utilities.Factory.LocalPort();
+                Int32 port = Int32.Parse(getport);
+                IPAddress localAddr = IPAddress.Parse(getip); 
+
+                // TcpListener server = new TcpListener(port);
+                Console.WriteLine("Server: Starting now to listen to: " + getip + ":" + getport);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+                ListeningLoop:
+                // Buffer for reading data
+                Byte[] bytes = new Byte[10000];
+                String data = null;
+
+                // Enter the listening loop. 
+                while (true)
+                {
+                    Console.Write("Server: Waiting for a connection... ");
+
+
+                    // Perform a blocking call to accept requests. 
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Server: Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+                    Console.WriteLine("A connection from: {0} - Port: {1}", client.Client.RemoteEndPoint.ToString(),
+                                      ((IPEndPoint) client.Client.RemoteEndPoint).Port.ToString());
+                    Console.WriteLine("Has been establisehd to this server: {0} - Port: {1} ",
+                                      client.Client.LocalEndPoint.ToString(),
+                                      ((IPEndPoint) client.Client.LocalEndPoint).Port.ToString());
+                    //Console.WriteLine(client.Client.LocalEndPoint.ToString());
+                    bool secondCycle = false;
+                    redo:
+                    int i;
+
+                    // Loop to receive all the data sent by the client. 
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        String data2 = data;
+                        if (!secondCycle) data = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n0";
+
+                        if (!secondCycle)
+                        {
+                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                            // Send back a response.
+                            stream.Write(msg, 0, msg.Length);
+
+                            Console.WriteLine("Server with IP: {1}:{2} - has Sent this: {0}", data, getip, getport);
+                        }
+                        if (data2.ToUpper().Contains("POST")) //teises ringis ei ole enam POST sees, läeb edasi
+                        {
+                            secondCycle = true;
+                            goto redo;
+                        }
+
+                        //data2 ON SEE KUS oige p2ring veel sees
+
+
+                        if (!data2.ToUpper().Contains("GET")) SetText1(data2 + ";");
+
+                        if (data2.ToUpper().Contains("GET") && data2.ToUpper().Contains("fullfilename"))
+                        {
+
+                            Console.WriteLine("Server: GOT DOWNLOAD REQUEST FROM ASKER");
+                            if (Directory.Exists(@"C:\"))
+                            {
+                                //DownloadManager.SendFile(@"C:\wazaa\");
+                                //TCP listener saab get query ja stardib /getfile=fname ja IP kuhu saata ja saadab wazaa kaustast faili
+                                Console.WriteLine("Server: GOT DOWNLOAD REQUEST FROM ASKER");
+                            }
+                            else if (Directory.Exists(@"D:\"))
+                            {
+
+                            }
+                        }
+
+
+                        if (data2.ToUpper().Contains("GET"))
+                        {
+                            Utilities.filterAndDistributeQuery(data2, RichTextBox);
+                        }
+                        break;
+
+                    }
+                    Console.WriteLine("Server: Shutdown and end connection");
+                    client.Close();
+                    Console.WriteLine("Server: Initalizing for new incomming requests........");
+
+                    goto ListeningLoop;
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Server: SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+                Console.WriteLine("server: FINAL SERVER S>Top");
+
+            }
+
+
+        }
+
+        private delegate void SetTextCallback(string text);
+
+        private void SetText1(string text)
+        {
+            if (this.RichTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText1);
+                RichTextBox.Invoke(d, new object[] {text});
+            }
+            else
+            {
+                this.RichTextBox.Text += (text);
+            }
+        }
+    }
+}
+
---
+++ Peer2Peer/Peer2Peer/utilities/Factory.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peer2Peer.utilities
+{
+    public class Factory
+    {
+        public string PostJson { get; set; }
+
+        public static string LocalIPAddress()
+        {
+            var ip = string.Empty;
+            var re = System.IO.File.ReadLines("Config.txt").FirstOrDefault();
+            if (re != null)
+                ip = re.Split(null).FirstOrDefault();
+            return ip;
+        }
+
+        public static string LocalPort()
+        {
+            var port = string.Empty;
+            var re = System.IO.File.ReadLines("Config.txt").FirstOrDefault();
+            if (re != null)
+                port = re.Split(null).LastOrDefault();
+            return port;
+        }
+
+        public static List<string> FindFile(string fileName)
+        {
+            var stringList = new List<string>();
+            string curFile = string.Empty;
+            if (Directory.Exists(@"C:\wazaa"))
+            {
+                DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(@"c:\wazaa\");
+                FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + fileName + "*.*");
+
+                foreach (FileInfo foundFile in filesInDir)
+                {
+
+                    stringList.Add(foundFile.Name);
+                }
+            }
+            else if (Directory.Exists(@"D:\wazaa"))
+            {
+                DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(@"D:\wazaa\");
+                FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + fileName + "*.*");
+
+                foreach (FileInfo foundFile in filesInDir)
+                {
+                    string fullName = string.Format(foundFile.Directory + @"\" + foundFile.Name);
+                    stringList.Add(foundFile.Name);
+                }
+            }
+
+            return stringList;
+
+        }
+
+        public static RequestModel ParseRequest(string reuquestString)
+        {
+            var request = new RequestModel();
+            request.Name =
+                HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("name");
+            request.Sendip = HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("sendip");
+            request.Sendport = HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("sendport");
+            request.TimeToLive =
+                HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("ttl");
+            request.Id =
+                HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("id");
+            request.Noask =
+                HttpUtility.ParseQueryString(reuquestString.Substring(new[] { 0, reuquestString.IndexOf('?') }.Max()))
+                    .Get("noask")
+                    .Split(new char[] { '_' })
+                    .ToList();
+            return request;
+        }
+
+        public static List<KeyValuePair<IPAddress, string>> getIpsAndPorts()
+        {
+            List<KeyValuePair<IPAddress, string>> clientList = new List<KeyValuePair<IPAddress, string>>();
+            string[] clients = System.IO.File.ReadAllLines("IP&Port.txt");
+            var sth = JArray.Parse(File.ReadAllText("IP&Port.txt"));
+
+            foreach (var client in sth.ToList())
+            {
+                IPAddress Ip = null;
+                string port = "";
+                if (!string.IsNullOrEmpty(client.First.Value<string>()))
+                    Ip = IPAddress.Parse(client.First.Value<string>());
+                if (!string.IsNullOrEmpty(client.Last.Value<string>()))
+                    port = client.Last.Value<string>();
+                if (Ip != null && !string.IsNullOrEmpty(port))
+                    clientList.Add(new KeyValuePair<IPAddress, string>(Ip, port));
+            }
+            return clientList;
+        }
+
+        public static void filterAndDistributeQuery(string data, RichTextBox textbox)
+        {
+            var startParsing = Utilities.ParseRequest(FilterQuery.getAllParametersFromGetRequest(data));
+            var ttl = Int32.Parse(startParsing.TimeToLive) - 1;
+            var foundFiles = Utilities.FindFile(startParsing.Name);
+            var fileList = new List<FileModel>();
+            if (!foundFiles.Any() && ttl > 0)
+            {
+                var places = Utilities.getIpsAndPorts();
+                foreach (var keyValuePair in places)
+                {
+                    if (startParsing.Noask.Contains(keyValuePair.Key.ToString()))
+                        return;
+                    var requestModel = startParsing;
+                    requestModel.TimeToLive = ttl.ToString();
+                    requestModel.Noask.Add(LocalIPAddress());
+                    var request = new Request(requestModel, keyValuePair.Key.ToString(), keyValuePair.Value.ToString(), textbox);
+                    Thread worker = new Thread(request.doRequest);
+                    worker.IsBackground = true;
+                    worker.SetApartmentState(System.Threading.ApartmentState.STA);
+                    worker.Name = "ParseRequestsAndSendRequestsTTL";
+                    worker.Start();
+                }
+            }
+            else if (foundFiles.Any() && ttl > 0)
+            {
+                foreach (var file in foundFiles)
+                {
+                    fileList.Add(new FileModel { Ip = LocalIPAddress(), Port = LocalPort(), Name = file });
+                }
+                var json = JsonConvert.SerializeObject(new PostObject { Id = String.Empty, Files = fileList });
+                var postreq = new PostRequestHandler(json, startParsing.Sendip, startParsing.Sendport);
+                //postreq.postRequest();
+                Thread worker = new Thread(postreq.postRequest);
+                worker.IsBackground = true;
+                worker.SetApartmentState(System.Threading.ApartmentState.STA);
+                worker.Name = "PostRequestThreadHE";
+                worker.Start();
+
+            }
+            else
+            {
+                //TODO:send error sest ttl on l'bi
+            }
+        }
+
+        public bool IsThisGetOrPost(string data)
+        {
+            return !FilterQuery.getAllParametersFromGetRequest(data).Contains("GET");
+        }
+
+
+
+    }
+}
